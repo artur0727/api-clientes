7c50f0b8a70520f6dafcae53014fec150079b351
arturo <arturo.h.0792@gmail.com>
2023-05-04 15:21:05 -0300

    Rutas del controlador Cliente

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,47 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| Aquí es donde puede registrar rutas API para su aplicación. 
+| Estas rutas las carga el RouteServiceProvider y todas ellas 
+| se asignarán al grupo de middleware "api".
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+/*
+ *
+ *Crear un nuevo Cliente
+ *
+*/
+
+// POST Endpoint: /crearcliente
+Route::post('/crearcliente', [ClienteController::class, 'crearCliente']);
+
+
+/**
+ *  
+ * Muestra una lista de clientes con Promedio de Edad entre todos los clientes
+ * Muestra la Desviación estándar entre las edades de todos los clientes
+ * 
+*/
+
+// GET Endpoint: /listclientes
+Route::get('/listclientes', [ClienteController::class, 'listarClientes']);
+
+
+/**
+ * 
+ * Mostra una lista de clientes con Promedio de Edad entre todos los clientes
+ * Muestra la Desviación estándar entre las edades de todos los clientes
+ * 
+*/
+
+// GET Endpoint: /kpideclientes
+Route::get('/kpideclientes', [ClienteController::class, 'obtenerPromedioYDesviacion']);

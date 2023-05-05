90d49c7e1cbf3b7eef8c8f870bb6bd96f81dfb36
arturo <arturo.h.0792@gmail.com>
2023-05-05 11:45:17 -0300

    redireccion a la documentacion del swagger

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://app.swaggerhub.com/apis-docs/artur0727/challenge-clientes-arturo/1.0.0');
 });
